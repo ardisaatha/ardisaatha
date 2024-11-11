94353f261233caa1d23f2928ed6eeda079deefe3
Atha Ardisa <92588716+ardisaatha@users.noreply.github.com>
2024-11-11 15:30:14 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
 const test = console.log("testing")
 const look = console.log(test, "github merge to main check history")
+const temporary = console.log("this file was a temporary to check the commit history")
+// const push to main tommorow
